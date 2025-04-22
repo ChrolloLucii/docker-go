1260578588a1474b520e5549951b2dc49a3c59a1
jammbeast <jammbeast69@mail.ru>
2025-04-22 03:06:24 +0300

    feat: implement error handling middleware and update index.js to use it

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import errorMiddleware from "./src/middlewares/errorMiddleware.js";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
@@ -11,6 +14,8 @@ const PORT = process.env.PORT || 4000;
 app.get("/api/health", (req, res) => {
   res.json({ status: "OK" });
 });
+
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log(`🚀 Backend listening on http://localhost:${PORT}`);
---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,22 @@
+import AuthService from '../services/authService.js';
+export async function register(req,res, next){
+    try {
+        const {username, email, password} = req.body;
+        const user = await AuthService.register({username, email, password});
+        res.status(200).json(user);
+    }
+    catch{err}{
+        next(err);
+    }
+}
+
+export async function login(req,res,next) {
+    try { 
+        const {email, password} = req.body;
+        const result = await AuthService.login({email, password});
+        res.json(result);
+    }
+    catch(err){
+        next(err);
+    }
+}
